b64d9ca0f0a5f51fc187737dc94800fc039c0218
narayanvashistha <vashisthanarayan98@gmail.com>
2025-08-13 12:43:35 +0530

    Missing entities in user, user repo updated

--- src/main/java/com/system_mastery/ascension/model/User.java
+++ src/main/java/com/system_mastery/ascension/model/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
     @Setter
@@ -21,9 +22,18 @@ public class User {
     @Setter
     private String password;
 
+    @Setter
+    private int xp = 0;
+
     @Column(unique = true)
     @Setter
     private String email;
+
+    @Setter
+    private int streak;
+
+    @Setter
+    private LocalDate lastStreakDate;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
